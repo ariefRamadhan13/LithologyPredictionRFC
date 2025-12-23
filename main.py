985b63fc2d99f0fce6b3eb63d54cadf834e5b39e
ariefRamadhan13 <arieframadhon1313@gmail.com>
2025-12-23 23:02:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-# home.py
 import streamlit as st
-from PIL import Image
 
-img = Image.open("Logo_Institut_Teknologi_Bandung (1).png")
-st.image(img,width = 1500)
+# Judul aplikasi
+st.title("Prediksi Litologi dari Data Mudlog")
 
-def main():
-    # Judul aplikasi utama
-    st.title("Prediksi Litologi dari MudLog Data")
-    
-    # Pilihan aplikasi yang ingin dijalankan
-    app_choice = st.selectbox("Pilih Aplikasi", ["Prediksi Harga Minyak (ARIMA Model)", "Prediksi Harga Minyak (LSTM Model)","Prediksi Harga Minyak (RNN Model)", "Analisis Downhole P&T (RF)", "Prediksi Litologi (RF)"])
-    
-    if app_choice == "Prediksi Harga Minyak (ARIMA Model)":
-        # Menjalankan aplikasi ARIMA dari Tes2.py
-        print('Hello World')
+# Deskripsi singkat aplikasi
+st.write("""
+Aplikasi ini digunakan untuk memprediksi litologi berdasarkan data mudlog. 
+Silakan lanjutkan dengan mengunggah dua file untuk analisis.
+""")
 
-if __name__ == "__main__":
-    main()
+# Tombol untuk lanjut ke halaman berikutnya
+if st.button('Next'):
+    st.write("Tombol Next ditekan! Lanjutkan ke halaman upload file.")  # Bisa diganti dengan kode untuk halaman berikutnya
